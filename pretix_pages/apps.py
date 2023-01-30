c97b3a5778118ed0ba4015b77daf02e1d4add872
Raphael Michel <michel@rami.io>
2023-01-30 15:00:20 +0100

    Bump to 1.5.0

--- pretix_pages/apps.py
+++ pretix_pages/apps.py
@@ -1,6 +1,7 @@
 from django.apps import AppConfig
 from django.utils.translation import gettext_lazy as _
 from . import __version__
+        compatibility = "pretix>=4.16.0"
 
 
 class PluginApp(AppConfig):
@@ -16,6 +17,7 @@ class PluginApp(AppConfig):
         featured = True
         visible = True
         version = __version__
+        compatibility = "pretix>=4.16.0"
 
     def ready(self):
         from . import signals  # NOQA
